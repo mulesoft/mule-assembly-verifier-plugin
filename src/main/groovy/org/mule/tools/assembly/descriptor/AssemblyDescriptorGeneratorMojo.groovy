b835d0002a4abbe4c05f175d1e385d020e78ca9a
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2021-07-21 12:00:07 -0500

    DEL-3317: Add the runtime descriptor generation mechanism on the distro assembly
    - Code review adjustments

--- src/main/groovy/org/mule/tools/assembly/descriptor/AssemblyDescriptorGeneratorMojo.groovy
+++ src/main/groovy/org/mule/tools/assembly/descriptor/AssemblyDescriptorGeneratorMojo.groovy
@@ -1,4 +1,9 @@
-// @todo[question]: Should we add any copyright headers
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 // @todo: Skip HelpMojo sources from coverage report
 
 package org.mule.tools.assembly.descriptor
